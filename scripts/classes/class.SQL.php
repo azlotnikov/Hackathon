c9041b18237fd0dc37eb3299e40bad008ce1eb00
Mark Tertishniy <mtertishniy@gmail.com>
2014-03-29 01:47:30 +1100

    Add GetCallQuery function to sql class

--- scripts/classes/class.SQL.php
+++ scripts/classes/class.SQL.php
@@ -78,6 +78,11 @@ class SQL
          . " WHERE  $field = ?";
    }
 
+   public static function GetCallQuery($func_name, $param_amount = 0)
+   {
+      return sprintf('CALL %s(%s)', $func_name, echo ($am > 0 ? '?' : '') . str_repeat(',?', $am - ($am != 0)));
+   }
+
    public static function PrepareFieldsForSelect($table, $fields)
    {
       $result = Array();
