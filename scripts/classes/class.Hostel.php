61459284ce37b27a56425f7f22309379a6613f3e
ZRazor <exp.razor@gmail.com>
2014-03-28 22:28:10 +1100

    Add base classes (empty)

---
+++ scripts/classes/class.Hostel.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.Entity.php';
+
+class Hostel extends Entity
+{
+
+}
---
+++ scripts/classes/class.Room.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.Entity.php';
+
+class Room extends Entity
+{
+
+}
