b62762bb80194d8e910866610aa7b95e5d0e6bfd
Mark Tertishniy <mtertishniy@gmail.com>
2014-03-29 01:37:29 +1100

    Add routing in index

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/container.php';
 
 switch ($request[0]) {
-   case '':
+   case '': case null: case false:
+      SetActiveItem();
+      break;
+
+   case 'login':
+      SetActiveItem('login');
+      require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/login.php';
+      break;
+
+   case 'registration':
+      SetActiveItem('registration');
+      require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/registration.php';
+      break;
+
+   case 'profile':
+      SetActiveItem('profile');
+      // require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/profile.php';
       break;
 
    default:
