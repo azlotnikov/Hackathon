ee1700dda35858561f6cf33d57ccc39b10df82f7
Mark Tertishniy <mtertishniy@gmail.com>
2014-03-29 01:54:14 +1100

    Add login(without page)

--- scripts/login.php
+++ scripts/login.php
@@ -2,14 +2,14 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.DataHandling.php';
 
 $fromUri = isset($_GET['originating_uri']) ? $_GET['originating_uri'] : '/profile';
-if (Authentification::checkCredentials()) {
-   header("Location: /profile");
-}
+
+if (Authentification::CheckCredentials()) Redirect('/profile');
+
 if (isset($_POST['submit'])) {
    $bool                 = isset($_SESSION['attempts']);
    $_SESSION['attempts'] = $bool ? $_SESSION['attempts'] + 1 : 1;
    $post                 = GetPOST();
-   $email                = $post['mail'];
+   $login                = $post['login'];
    $pass                 = $post['pass'];
    try {
       //captcha checking
@@ -23,30 +23,21 @@ if (isset($_POST['submit'])) {
             throw new Exception('');
          }
       }
-      $data_h = new DataHandling();
       try {
-         $data_h->validateEmail($email, ERROR_LOGIN)
-                ->validatePassword($pass, ERROR_LOGIN);
-      } catch (Exception $e) {
-         $errorMsg = $e->getMessage();
-         throw new Exception('');
-      }
-      try {
-         AuthorizedUser::Login($email, $pass);
+         (new DataHandling())->validatePassword($pass, ERROR_LOGIN);
+         AuthorizedUser::Login($login, $pass);
          $_SESSION['attempts'] = null;
-         header("Location: $fromUri");
+         Redirect('/profile');
       } catch (Exception $e) {
          $errorMsg = $e->getMessage();
       }
    } catch (Exception $e) {}
-      // $errorMsg = $e->getMessage();
 }
-if (isset($_SESSION['attempts']) && $_SESSION['attempts'] >= NUMBER_OF_LOGIN_ATTEMPTS) {
-   $smarty->assign('hasCaptcha', 'true');
-}
-$smarty->assign('email', isset($email) ? $email : '')
-       ->assign('fromUri', $fromUri)
-       ->assign('captcha_img_url', '/kcaptcha/captcha.php?' . session_name() . '=' . session_id())
+$smarty->assign('fromUri', $fromUri)
+       ->assign('login', isset($login) ? $login : '')
+       ->assign('captcha_img_url', sprintf('/kcaptcha/captcha.php?%s=%s', session_name(), session_id()))
+       ->assign('hasCaptcha', isset($_SESSION['attempts']) && $_SESSION['attempts'] >= NUMBER_OF_LOGIN_ATTEMPTS)
        ->assign('errorMsg', isset($errorMsg) ? $errorMsg : null)
        ->display('login.tpl');
+
 unset($_SESSION['captcha_keystring']);
