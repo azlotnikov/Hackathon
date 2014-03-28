038353b65ddd90726e240ab3fb82ec48f9b0d259
ZRazor <exp.razor@gmail.com>
2014-03-28 22:55:09 +1100

    base structure for class.Room

--- scripts/classes/class.Floor.php
+++ scripts/classes/class.Floor.php
@@ -4,5 +4,13 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.Entity.php';
 
 class Floor extends Entity
 {
+    const TABLE = 'floors';
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->fields = Array(
+
+        );
+    }
 }
--- scripts/classes/class.Room.php
+++ scripts/classes/class.Room.php
@@ -4,5 +4,40 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.Entity.php';
 
 class Room extends Entity
 {
+   const NUMBER_FLD      = 'number';
+   const FLOOR_FLD       = 'floor_id';
+   const LAST_UPDATE_FLD = 'last_update';
+   const POLYGON_FLD     = 'polygon';
 
+   const TABLE = 'rooms';
+
+   public function __construct()
+   {
+      parent::__construct();
+      $this->fields = Array(
+         $this->idField,
+         new Field(
+            static::NUMBER_FLD,
+            IntType(),
+            true,
+            'Номер',
+            Array(Validate::IS_NUMERIC)
+         ),
+         new Field(
+            static::LAST_UPDATE_FLD,
+            TimestampType(),
+            true
+         ),
+         new Field(
+            static::FLOOR_FLD,
+            IntType(),
+            true
+         ),
+         new Field(
+            static::POLYGON_FLD,
+            StrType(500),
+            true
+         )
+      );
+   }
 }
