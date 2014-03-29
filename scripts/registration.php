89658e9afcfee4249d9e5bf303f7ddd9e39c38e4
Mark Tertishniy <mtertishniy@gmail.com>
2014-03-29 12:32:39 +1100

    Delete nickname from registration

--- scripts/registration.php
+++ scripts/registration.php
@@ -1,26 +1,25 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.User.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/classes/class.DataHandling.php';
-// require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/lib/reg_auth.inc';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/lib/reg_auth.inc';
 
-echo "reg";
 if (isset($_POST['submit'])) {
    $post     = GetPOST();
    $smarty->assign(
       $fields = [
          User::NAME_FLD     => $post['name'],
          User::LOGIN_FLD    => $post['login'],
-         User::SURNAME_FLD  => $post['surname'],
-         User::NICKNAME_FLD => $post['nickname']
+         User::SURNAME_FLD  => $post['surname']
       ]
    );
    $pass   = $post['pass'];
    $repass = $post['repass'];
    try {
-      (new DataHandling())->validatePassword($pass)
-                          ->validateRepeatPasswords($pass, $repass);
-      Registration::Register($fields);
-      Redirect('/success_register');
+      (new DataHandling())->ValidateLogin($post['login'])
+                          ->ValidatePassword($pass)
+                          ->ValidateRepeatPasswords($pass, $repass);
+      Registration::Register($fields, $pass);
+      // Redirect('/success_register');
    } catch (Exception $e) {
       $smarty->assign('db_error', $e->getMessage());
    }
