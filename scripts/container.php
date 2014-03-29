9db52f2a46b67fb78a313589adadb32085e7d8a5
Mark Tertishniy <mtertishniy@gmail.com>
2014-03-29 12:32:21 +1100

    Fix auth

--- scripts/container.php
+++ scripts/container.php
@@ -7,4 +7,8 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/constants.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/settings.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/utils.php';
 
+require_once $_SERVER['DOCUMENT_ROOT'] . '/scripts/lib/reg_auth.inc';
+
 $request = explode('/', substr($_SERVER['REQUEST_URI'], 1));
+
+$smarty->assign('isLogin', Authentification::CheckCredentials());
