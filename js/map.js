386856d98f8317d6fa8ef523cc66d4eca3456a95
ZRazor <exp.razor@gmail.com>
2014-03-31 05:19:44 +1100

    Revert "Add floor select"
    This reverts commit 1cccf1dca1273024179e822e585dfd26a39ea5c2.

--- js/map.js
+++ js/map.js
@@ -40,7 +40,17 @@ var min_scale = 0.1,  //минимальный масштаб карты
     littleCircleRadius = 10;   //радиус маленького кружка события
 
 function Map() {               //самый главный объект карта
-
+    this.scale = 0.7;         //начальный масштаб
+    this.places = {};          //массив всех мест: ключ - id места
+    this.events = {};          //массив всех событий
+//  this.activePlace  = {};
+    this.cachedEvents = {};
+    this.stage = new Kinetic.Stage({        //канвас
+        container: 'container',
+        width: $(document).width() - 100,
+        height: $(document).height() - 100,
+        draggable: true
+    });
 }
 
 function loadIcons() {
@@ -60,38 +70,12 @@ function loadIcons() {
     eventsObjects["leisure"].imageIcon.src = '/img/icon_leisure.png';
 }
 
-Map.prototype.init = function (floor) {
-    this.scale = 0.7;         //начальный масштаб
-    this.places = {};          //массив всех мест: ключ - id места
-    this.events = {};          //массив всех событий
-    this.cachedEvents = {};
-    this.floor = floor;
-    this.stage = new Kinetic.Stage({        //канвас
-        container: 'container',
-        width: $(document).width() - 20, //TODO replace with normal?!!!
-        height: $(document).height() - 20,
-        draggable: true
-    });
+Map.prototype.init = function () {
     this.getInitInfo();                             //получение информации обо всем на карте ajax
     // console.log("OO " + Date());
     // this.lastUpdatedDate = Date();
     // console.log("OOO " + this.lastUpdatedDate);
     setInterval(this.getNewInfo, UPDATE_INTERVAL);
-
-    if (this.eventsLayer) {
-        this.eventsLayer.removeChildren();
-        delete this.eventsLayer;
-    }
-
-    if (this.placesLayer) {
-        this.placesLayer.removeChildren();
-        delete this.placesLayer;
-    }
-
-    if (this.imageLayer) {
-        this.imageLayer.removeChildren();
-        delete this.imageLayer;
-    }
 
     var layer = new Kinetic.Layer(),
         imageObj = new Image();
@@ -118,7 +102,7 @@ Map.prototype.init = function (floor) {
         map.imageLayer.draw();
 
         layer.draw();
-        map.initPlaces();
+        map.initPlaces();       //нарисовать скрытый слой с местами
         loadIcons();
         handleEventsLayers();   //определяет какие события из чекбоксов нарисовать
 
@@ -128,7 +112,7 @@ Map.prototype.init = function (floor) {
         });
     };
 
-    imageObj.src = '/img/map.jpg'; //TODO add floor loading
+    imageObj.src = '/img/map.jpg';
 
 
 };
@@ -140,7 +124,7 @@ Map.prototype.getInitInfo = function () {
         url: '/scripts/handlers/handler.Map.php',
         data: {
             action: "getInitInfo",
-            floor: this.floor,
+            floor: "1",
             hostel: '1'
         },
         success: function (data) {
@@ -169,8 +153,7 @@ Map.prototype.getNewInfo = function () {
         url: '/scripts/handlers/handler.Map.php',
         data: {
             action: "getNewInfo",
-            last_updated_date: map.lastUpdatedDate,
-            floor: this.floor
+            last_updated_date: map.lastUpdatedDate
         },
         success: function (data) {
             console.log(data);
@@ -515,7 +498,7 @@ function clearEventForm() {
 }
 
 $(function () {
-    map.init($('#floor_select').val());
+    map.init();
     map.lastUpdatedDate = getCurrentDate();
 
     $('#layers input[name="events_layer"]').change(handleEventsLayers);
@@ -541,12 +524,6 @@ $(function () {
     $('#event_datetime').datetimepicker({
         lang: 'ru',
         format: 'Y-m-d H:i:s'
-    });
-
-    $('#floor_select').change(function () {
-        map.init($('#floor_select').val());
-        handleEventsLayers();
-        clearEventForm();
     });
 
     $('#event_type').change(function () {
